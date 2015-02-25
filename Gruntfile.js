b623212270ca99218cd9c56a6f8e179f35ba4ce4
rediris <romanedirisinghe@gmail.com>
2015-02-24 23:56:20 -0500

    Basic theme files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,96 +2,73 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
-    // settings
-    temp: '.temp',
-
     sass: {
       options: {
-        includePaths: ['<%= temp %>/bower_components/foundation/scss'],
+        includePaths: ['.temp/bower_components/foundation/scss']
       },
       dist: {
         options: {
-          outputStyle: 'compressed',
+          outputStyle: 'compressed'
         },
         files: {
-          '<%= temp %>/css/_style.css': 'scss/style.scss',
-        },
-      },
-    },
-
-    autoprefixer: {
-      options: {
-        browsers: ['last 3 versions','> 5%','Firefox ESR','ie 9'],
-      },
-      target: {
-        expand: true,
-        flatten: true,
-        src: '<%= temp %>/css/*.css',
-        dest: '<%= temp %>/css/',
-      },
+          '.temp/_style.scss': 'scss/style.scss'
+        }
+      }
     },
 
     copy: {
       scripts: {
         expand: true,
-        cwd: '<%= temp %>/bower_components/',
+        cwd: '.temp/bower_components/',
         src: '**/*.js',
-        dest: '<%= temp %>/js',
+        dest: '.temp/js'
       },
 
       maps: {
         expand: true,
-        cwd: '<%= temp %>/bower_components/',
+        cwd: '.temp/bower_components/',
         src: '**/*.map',
-        dest: '<%= temp %>/js',
-      },
+        dest: '.temp/js'
+      }
     },
 
     uglify: {
       dist: {
         files: {
-          'js/modernizr/modernizr.min.js': ['<%= temp %>/js/modernizr/modernizr.js'],
-          '<%= temp %>/js/infrastrukt.min.js': ['js/infrastrukt.js'],
-        },
-      },
+          'js/modernizr/modernizr.min.js': ['.temp/js/modernizr/modernizr.js'],
+          '.temp/js/arquitecta.min.js': ['js/arquitecta.js']
+        }
+      }
     },
 
     concat: {
       js: {
         options: {
-          separator: ';',
+          separator: ';'
         },
         src: [
-          '<%= temp %>/js/foundation/js/foundation.min.js',
-          '<%= temp %>/js/infrastrukt.min.js',
+          '.temp/js/foundation/js/foundation.min.js',
+          '.temp/js/arquitecta.min.js'
         ],
-        dest: 'js/app.js',
+        dest: 'js/app.js'
       },
       css: {
         src: [
             'scss/theme/_comment-block.scss',
-            '<%= temp %>/css/_style.css',
+            '.temp/_style.scss'
         ],
-        dest: 'style.css',
-      },
+        dest: 'style.css'
+      }
     },
 
     watch: {
       grunt: { files: ['Gruntfile.js'] },
+
       sass: {
         files: 'scss/**/*.scss',
-        tasks: ['sass','copy','uglify','concat'],
-        options: {
-          livereload: 35729,
-        },
-      },
-      php: {
-        files: ['**/*.php'],
-        options: {
-          livereload: 35729,
-        },
-      },
-    },
+        tasks: ['sass', 'copy' , 'uglify', 'concat']
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-sass');
@@ -99,8 +76,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-autoprefixer');
 
-  grunt.registerTask('build', ['sass', 'autoprefixer', 'copy', 'uglify', 'concat']);
-  grunt.registerTask('default', ['watch', 'build']);
+  grunt.registerTask('build', ['sass', 'copy', 'uglify', 'concat']);
+  grunt.registerTask('default', ['watch','build']);
 };
--- README.md
+++ README.md
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-# Infrastrukt
+# Arquitecta
+Wordpress child theme, based on [Infrastrukt](http://github.com/rediris/infrastrukt)
 
-Infrastrukt is WordPress theme built on [Foundation 5](http://foundation.zurb.com/), by [Zurb](http://zurb.com/).
-
-- Big props go to [drewsymo](http://github.com/drewsymo/) for his work on the [Foundation for WordPress](https://github.com/drewsymo/Foundation) theme. Infrastrukt was originally forked from the FWP repository.
-
-- Infrastrukt features a resource loader, which will allow loading of JS assets locally or via CDN.
-
-- CSS is now generated via SASS.
-
-## Note
-- This README is only partially up-to-date.
 
 ## Developer use:
 
@@ -23,13 +13,9 @@ In the infrastrukt directory, run these commands:
 - `bower prune`
 
 ### Grunt Tasks
-- `grunt build` (compiles SASS to CSS, minifies CSS and JS files)
-- `grunt` (default watch task, compiles SASS to CSS, minifies CSS and JS files)
+- `grunt build` (initial build: compiles SASS to CSS, minifies CSS and JS files)
+- `grunt` (default watch task, executes build upon changes)
 
 ### Adding Additional Dependencies
 - JavaScript library: `bower install -S [package]`
 - NodeJS package: `npm install --save-dev [package]`
-=======
-# arquitecta
-Wordpress child theme, based on @infrastrukt
->>>>>>> Initial commit
