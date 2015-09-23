c973449565f1f59fc50624bff112e923593070fa
rediris <romanedirisinghe@gmail.com>
2015-09-22 20:02:16 -0400

    Merge Infrastrukt master

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,73 +2,96 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
+    // settings
+    temp: '.temp',
+
     sass: {
       options: {
-        includePaths: ['.temp/bower_components/foundation/scss']
+        includePaths: ['<%= temp %>/bower_components/foundation/scss'],
       },
       dist: {
         options: {
-          outputStyle: 'compressed'
+          outputStyle: 'compressed',
         },
         files: {
-          '.temp/_style.scss': 'scss/style.scss'
-        }
-      }
+          '<%= temp %>/css/_style.css': 'scss/style.scss',
+        },
+      },
+    },
+
+    autoprefixer: {
+      options: {
+        browsers: ['last 3 versions','> 5%','Firefox ESR','ie 9'],
+      },
+      target: {
+        expand: true,
+        flatten: true,
+        src: '<%= temp %>/css/*.css',
+        dest: '<%= temp %>/css/',
+      },
     },
 
     copy: {
       scripts: {
         expand: true,
-        cwd: '.temp/bower_components/',
+        cwd: '<%= temp %>/bower_components/',
         src: '**/*.js',
-        dest: '.temp/js'
+        dest: '<%= temp %>/js',
       },
 
       maps: {
         expand: true,
-        cwd: '.temp/bower_components/',
+        cwd: '<%= temp %>/bower_components/',
         src: '**/*.map',
-        dest: '.temp/js'
-      }
+        dest: '<%= temp %>/js',
+      },
     },
 
     uglify: {
       dist: {
         files: {
-          'js/modernizr/modernizr.min.js': ['.temp/js/modernizr/modernizr.js'],
-          '.temp/js/arquitecta.min.js': ['js/arquitecta.js']
-        }
-      }
+          'js/modernizr/modernizr.min.js': ['<%= temp %>/js/modernizr/modernizr.js'],
+          '<%= temp %>/js/infrastrukt.min.js': ['js/infrastrukt.js'],
+        },
+      },
     },
 
     concat: {
       js: {
         options: {
-          separator: ';'
+          separator: ';',
         },
         src: [
-          '.temp/js/foundation/js/foundation.min.js',
-          '.temp/js/arquitecta.min.js'
+          '<%= temp %>/js/foundation/js/foundation.min.js',
+          '<%= temp %>/js/infrastrukt.min.js',
         ],
-        dest: 'js/app.js'
+        dest: 'js/app.js',
       },
       css: {
         src: [
             'scss/theme/_comment-block.scss',
-            '.temp/_style.scss'
+            '<%= temp %>/css/_style.css',
         ],
-        dest: 'style.css'
-      }
+        dest: 'style.css',
+      },
     },
 
     watch: {
       grunt: { files: ['Gruntfile.js'] },
-
       sass: {
         files: 'scss/**/*.scss',
-        tasks: ['sass', 'copy' , 'uglify', 'concat']
-      }
-    }
+        tasks: ['sass','copy','uglify','concat'],
+        options: {
+          livereload: 35729,
+        },
+      },
+      php: {
+        files: ['**/*.php'],
+        options: {
+          livereload: 35729,
+        },
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-sass');
@@ -76,7 +99,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-autoprefixer');
 
-  grunt.registerTask('build', ['sass', 'copy', 'uglify', 'concat']);
-  grunt.registerTask('default', ['watch','build']);
+  grunt.registerTask('build', ['sass', 'autoprefixer', 'copy', 'uglify', 'concat']);
+  grunt.registerTask('default', ['watch', 'build']);
 };
